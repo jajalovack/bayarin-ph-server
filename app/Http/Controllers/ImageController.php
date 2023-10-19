ecd0617d194f601227976a6078de781ea4215a05
jajalovack <dvtejuco@gmail.com>
2023-10-19 17:52:53 +0800

    debug view image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
 
 class ImageController extends Controller
 {
@@ -23,6 +24,8 @@ class ImageController extends Controller
     public function profile(string $filename)
     {
         $file = storage_path('app/public/images/' . $filename);
+        Log::info('File path: ' . $file);
+
         if (file_exists($file)) {
             $image = file_get_contents($file);
             $mime = mime_content_type($file);
