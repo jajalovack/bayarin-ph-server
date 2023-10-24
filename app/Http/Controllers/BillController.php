e2c3421e8b33b7b145291a3bf23ea3e50e7d061e
jajalovack <dvtejuco@gmail.com>
2023-10-25 01:56:12 +0800

    disabled universal list of bills and transactions

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -13,34 +13,34 @@ use App\Http\Resources\BillResource;
 
 class BillController extends Controller
 {
-    public function bills()
-    {
-        $bill=Bill::all();
-        $response=[];
-        for ($i=0;$i<count($bill);$i++)
-        {
-            $status=Billstatus::where('id',$bill[$i]->billstatus_id)->first()->status;
-            $transStatus=Transaction::where('bill_id',($i+1))->first();
+    // public function bills()
+    // {
+    //     $bill=Bill::all();
+    //     $response=[];
+    //     for ($i=0;$i<count($bill);$i++)
+    //     {
+    //         $status=Billstatus::where('id',$bill[$i]->billstatus_id)->first()->status;
+    //         $transStatus=Transaction::where('bill_id',($i+1))->first();
             
-            if($bill[$i]->status==3)
-            {
-                $payor=User::where('id',$transStatus->payor_id)->first();
-                $payorName=$payor->first_name.' '.$payor->last_name;
-                $status=$status.' '.$payorName;
-            }
-            array_push($response,[
-                'id'=>$bill[$i]->id,
-                'refnum'=>$bill[$i]->refnum,
-                'biller'=>Biller::where('id',$bill[$i]->biller_id)->first()->biller,
-                'category'=>Category::where('id',$bill[$i]->category_id)->first()->category,
-                'billed_to'=>$bill[$i]->billed_to,
-                'description'=>$bill[$i]->description,
-                'amount'=>$bill[$i]->amount,
-                'status'=>$status
-            ]);
-        }
-        return response($response,200);
-    }
+    //         if($bill[$i]->status==3)
+    //         {
+    //             $payor=User::where('id',$transStatus->payor_id)->first();
+    //             $payorName=$payor->first_name.' '.$payor->last_name;
+    //             $status=$status.' '.$payorName;
+    //         }
+    //         array_push($response,[
+    //             'id'=>$bill[$i]->id,
+    //             'refnum'=>$bill[$i]->refnum,
+    //             'biller'=>Biller::where('id',$bill[$i]->biller_id)->first()->biller,
+    //             'category'=>Category::where('id',$bill[$i]->category_id)->first()->category,
+    //             'billed_to'=>$bill[$i]->billed_to,
+    //             'description'=>$bill[$i]->description,
+    //             'amount'=>$bill[$i]->amount,
+    //             'status'=>$status
+    //         ]);
+    //     }
+    //     return response($response,200);
+    // }
 
     public function bill($refnum)
     {
--- routes/api.php
+++ routes/api.php
@@ -27,12 +27,12 @@ Route::post('/upload',[ImageController::class,'upload']);
 Route::group(['middleware'=>['auth:sanctum']],function() {
     Route::post('/logout',[AuthController::class,'logout']);
     Route::get('/profilepic/{filename}',[ImageController::class,'profile'])->where('filename','.*');
-    Route::get('/bills',[BillController::class,'bills']);
+    // Route::get('/bills',[BillController::class,'bills']);
     Route::get('/bill/{refnum}',[BillController::class,'bill']);
     Route::get('/billers',[BillerController::class,'billers']);
     Route::get('/profilepic/{filename}',[ImageController::class,'profile']);
     Route::get('/profile',[ProfileController::class,'viewProfile']);
-    Route::get('/alltransactions',[TransactionController::class,'alltransactions']);
+    // Route::get('/alltransactions',[TransactionController::class,'alltransactions']);
     Route::get('/transactions',[TransactionController::class,'transactions']);
     Route::get('/transaction/{id}',[TransactionController::class,'transaction']);
     Route::post('/pay',[TransactionController::class,'pay']);
